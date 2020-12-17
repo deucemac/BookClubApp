3b7be438f600a18afa520efa151ef67e14393349
Dwayne McDowell <dwaynemcdowell@Dwaynes-MacBook-Pro.local>
2020-12-17 17:13:32 -0500

    got content rendered

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,44 @@
 import AddBook from './AddBook'
 import './css/app.scss';
 import './App.css';
+import React, { useState, useEffect } from 'react'
 import { getAllBooks } from './services/api-method'
 
 function App() {
 
-  getAllBooks().then((data) => {
-    console.log(data)
-  }).catch(() => {
-    
-  })
+  // const [book, setBook] = useState(() => {
+  //   const books = getAllBooks();
+  //   books.then((data) => {
+  //     return data
+  //   }).catch((err)=> console.log(err))
+  //   return books;
+  // });
 
-  
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    getAllBooks().then((data) => {
+      setBooks(data)
+      // setBooks(prev => [...prev, data])
+    })
+  }, []);
+
+
+
+  console.log(books)
   return (
     <div className="App">
       <AddBook />
       <div className="books-container">
-        {/* {bookList} */}
+        {books.length && books.map((book, index) => (
+          <div key={index}>
+            <h1>{book.title}</h1>
+            <p>by {book.author}</p>
+            <img src={book.image} />
+            <p>{book.description}</p>
+          </div>
+        ))}
+
       </div>
     </div>
   );
